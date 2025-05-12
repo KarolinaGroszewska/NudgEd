d82cb4f84513b6b5806ba191889ef7d4596865b5
Ullas R <ullasr2004@gmail.com>
2025-05-12 15:06:44 +0530

    fixing mongoURI problem

--- server/src/db.js
+++ server/src/db.js
@@ -3,14 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const MONGODB_URI = process.env.MONGODB_URI;
-const connectionString = MONGODB_URI; 
+
 const connectToDB = async () => {
     try {
-        await mongoose.connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('Connected to MongoDB Atlas');} catch (error) {
+        await mongoose.connect(MONGODB_URI)
+        console.log('Connected to MongoDB Atlas');
+    } catch (error) {
         console.error('MongoDB connection error:', error);
     }
 }
