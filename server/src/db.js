8b5b106afec37b43056b8b816c579054585c354a
Karolina Groszewska <karolina.groszewska@gmail.com>
2025-05-13 16:37:23 -0500

    bug fix for PR

--- server/src/db.js
+++ server/src/db.js
@@ -1,16 +1,18 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
-const MONGODB_URI = process.env.MONGODB_URI;
-const connectionString = MONGODB_URI; 
+console.log(dotenv.config());
+var MONGODB_URI = process.env.MONGODB_URI; 
+
+if (!MONGODB_URI) {
+    console.error('MongoDB connection string not found in environment variables.');
+    process.exit(1);
+}
 const connectToDB = async () => {
     try {
-        await mongoose.connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('Connected to MongoDB Atlas');} catch (error) {
+        await mongoose.connect(MONGODB_URI)
+        console.log('Connected to MongoDB Atlas');
+    } catch (error) {
         console.error('MongoDB connection error:', error);
     }
 }
